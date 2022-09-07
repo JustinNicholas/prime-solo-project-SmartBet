d079f0e125374d312ef38a637396cd1f2a227b2a
Justin Nicholas <nicholasj964@yahoo.com>
2022-09-07 14:05:55 -0500

    calendar and time select for the manual add game is working and converting time to match time from API before we send to DB. need to work on sending all to DB.

--- src/components/AddBet/AddBet.jsx
+++ src/components/AddBet/AddBet.jsx
@@ -1,8 +1,13 @@
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from 'moment';
 
 function AddBet() {
+
     useEffect(() => {
         getTeamsFromDatabase();
     }, []);
@@ -16,15 +21,29 @@ function AddBet() {
         })
     }
 
+    const [startDate, setStartDate] = useState(new Date());
+
+    const dateChange = (date) => {
+        setStartDate(date);
+        console.log(moment(date).format());
+    }
+
+    const addBet = (event) => {
+        event.preventDefault();
+
+        console.log(startDate);
+    }
+
     return(
         <div>
             <h1>Add a bet!</h1>
+            <form onSubmit={() => addBet(event)}>
             <h3>Pick to win</h3>
             <select name="" id="">
                 <option value="0">Select a Team</option>
                 {teams.map( team => {
                     return(
-                        <option value={team}>{team.team_full_name}</option>
+                        <option key={team.id} value={team}>{team.team_full_name}</option>
                     )
                 })}
             </select>
@@ -34,16 +53,22 @@ function AddBet() {
                 <option value="0">Select a Team</option>
                 {teams.map( team => {
                     return(
-                        <option value={team}>{team.team_full_name}</option>
+                        <option key={team.id} value={team}>{team.team_full_name}</option>
                     )
                 })}
             </select>
+
             <input type="text" placeholder="Opponents moneyline"/>
             <h3>Date/Time</h3>
             {/* Would like this to be a calendar select and a time select... */}
-            <input type="text" placeholder="example 2022-09-08T16:00"/>
+            <DatePicker selected={startDate} showTimeSelect dateFormat="Pp" onChange={(date) => dateChange(date)} />
+            <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
 
 export default AddBet;
+
+// showTimeSelect dateFormat="Pp"
+//2022-09-08T20:20:00
