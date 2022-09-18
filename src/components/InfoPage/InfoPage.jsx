1d6e78e0ee0246b62d95c960a91371380179053e
Justin Nicholas <nicholasj964@yahoo.com>
2022-09-18 10:25:21 -0500

    more styling to info page.

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -5,8 +5,17 @@ function InfoPage() {
 
   return (
     <div className="info-page-container">
+      <div className='about-container'>
+        <h1 className='info-page-titles'>ABOUT SMARTBET</h1>
+        <img className='playbook-image greyscale-image' src={process.env.PUBLIC_URL + '/PlaybookImage/playbook.svg'} />
+        <div className='about-text'>
+          <p>Ever wonder how much money you've won or lost overtime through gambling? Smartbet is a web application that allows users to keep track of their personal NFL betting history. The ability to analyze and draw insights from personal betting statistics in SMARTBET enables users to make higher performing bets overtime.</p>
+          <p>The application also serves as a log to provide visibility into personal gambling history. With Smartbet, users are empowered to regulate their bets and protect against unhealthy gambling habits.</p>
+          <p>Smartbet was created and developed by Prime Digital Academy Student, Justin Nicholas, to showcase the skills and knowledge he acquired through his coursework.</p>
+        </div>
+      </div>
       <div className='tech-used-conatiner'>
-        <h1 className='info-page-titles'>Technologies Used</h1>
+        <h1 className='info-page-titles'>TECHNOLOGIES USED</h1>
         <div className='bubble-container'>
           <div className='tech-used-bubbles'>HTML</div>
           <div className='tech-used-bubbles'>CSS</div>
@@ -24,6 +33,11 @@ function InfoPage() {
         <h1 className='info-page-titles'>THANK YOU</h1>
         <div className='bubble-container'>
           <p className='thank-you-text'>Special thanks to my supoort system at hom an the Prime Academy Mitchison Cohort</p>
+        </div>
+        <div className='connect-container'>
+          <h1 className='info-page-titles'>LETS CONNECT!</h1>
+          <p className='linkedin-text'>Scan the QR Code below to visit my LinkedIn Page or go to <a className='linkedin-link' href="https://www.linkedin.com/in/justin-nicholas-p/">https://www.linkedin.com/in/ justin-nicholas-p/</a></p>
+          <img className='linkedin-image' src={process.env.PUBLIC_URL + '/LinkedInImage/linkedInQRCode.svg'} />
         </div>
       </div>
 
