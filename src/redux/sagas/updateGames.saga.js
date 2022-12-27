ce279a0aa4f876f6328ceec8efa2489041915b0d
Justin Nicholas <nicholasj964@yahoo.com>
2022-12-27 11:02:51 -0600

    removed old routes from client side after linking update to server side routes."

--- src/redux/sagas/updateGames.saga.js
+++ src/redux/sagas/updateGames.saga.js
@@ -5,21 +5,6 @@ function* updateGames() {
 
     try {
       yield axios.get('/database/updateData')
-    // Need to move this to the server side and use .then instead of yield
-        //clear out the table of games
-        // yield axios.delete('/database/games');
-        // clear out the table of week
-        // yield axios.delete('/database/week');
-        // GET week info from the 3rd party api.
-        // const week = yield axios.get('/api/week');
-        // POST the week info to the database with the response of week
-        // yield axios.post('/database/week', { week: week.data.currentWeek });
-          // console.log('game saga triggered');
-        // GET games info from the 3rd party api
-        // const response = yield axios.get('/api/games');
-          // console.log('sent get games to server');
-        // POST the games info to the database with the response of games response
-        // yield axios.post('/database/games', { games: response.data });
       } catch (error) {
         console.log('User get request failed', error);
       }
